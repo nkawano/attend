aa65881e800806ad1bd55ed7767e188752df79b0
kazutaka <kaz0830@gmail.com>
2011-02-23 00:47:28 +0900

    test

--- src/slim3/meta/MemberMeta.java
+++ src/slim3/meta/MemberMeta.java
@@ -1,6 +1,6 @@
 package slim3.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-02-20 15:51:32")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2011-02-23 00:44:32")
 /** */
 public final class MemberMeta extends org.slim3.datastore.ModelMeta<slim3.model.Member> {
 
