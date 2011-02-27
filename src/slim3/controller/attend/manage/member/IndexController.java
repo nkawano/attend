59d8ff3c8c9f1028e6e7f269f3d6614e93fcaa19
nkawano <nkawano58@gmail.com>
2011-02-28 01:10:24 +0900

    警告が嫌なのでインポートの編成を実行

--- src/slim3/controller/attend/manage/member/IndexController.java
+++ src/slim3/controller/attend/manage/member/IndexController.java
@@ -3,8 +3,6 @@ package slim3.controller.attend.manage.member;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
-import slim3.constants.Constants;
-
 public class IndexController extends Controller {
 
     @Override
